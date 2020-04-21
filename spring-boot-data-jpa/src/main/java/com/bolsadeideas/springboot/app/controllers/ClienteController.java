7f86f423b4a1888db24f5c59924742633d263f49
yazdebi <62398257+yazdebi@users.noreply.github.com>
2020-04-21 11:20:56 -0500

    Validacion mensajes de error
    Validación mensajes de error  para cada campo en form

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -2,10 +2,13 @@ package com.bolsadeideas.springboot.app.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -34,7 +37,12 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("titulo","Formulario de Cliente");
+			return "form";
+		}
+		
 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}
--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -8,10 +8,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "clientes")
@@ -20,17 +26,22 @@ public class Cliente implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotEmpty
+	@Size(min=4, max=12)
 	private String nombre;
+	@NotEmpty
 	private String apellido;
+	@NotEmpty
+	@Email
 	private String email;
+	
+	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
+	
 
 	private static final long serialVersionUID = 1L;
 
