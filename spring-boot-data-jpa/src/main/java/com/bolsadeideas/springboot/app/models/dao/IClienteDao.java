e82be9a6e97820198157623c85d6d16d36bf7acb
yazdebi <62398257+yazdebi@users.noreply.github.com>
2020-04-22 18:12:32 -0500

    Integración de CrudRepository de JPA
    Integración de CrudRepository de JPA

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -1,15 +1,10 @@
 package com.bolsadeideas.springboot.app.models.dao;
 
-import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
-	public List<Cliente> findAll(); 
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
 	
-	public void save(Cliente cliente);
-	
-	public Cliente findOne(Long id);
-	
-	public void delete(Long id);
 }
