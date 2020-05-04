df9d559fca0d69c2f9355bd7a155a2966bc86d9d
yazdebi <62398257+yazdebi@users.noreply.github.com>
2020-05-04 15:31:05 -0500

    SubirImagen httpRequest

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -11,13 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 	private final Logger log = LoggerFactory.getLogger(getClass());
-	@Override
+/*	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
 		log.info(resourcePath);
 		registry.addResourceHandler("/uploads/**").addResourceLocations(resourcePath);
-	}
+	}*/
 
 }
